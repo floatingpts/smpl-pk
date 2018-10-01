671c760accbb894eef26cf42cbae36a4c85d49a8
user <kellylotr@gmail.com>
2018-10-01 14:06:56 -0400

    added restframework to installed apps

--- isa-app/isa-app/settings.py
+++ isa-app/isa-app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Store music samples
+
+# MEDIA_URL = '/media/'
