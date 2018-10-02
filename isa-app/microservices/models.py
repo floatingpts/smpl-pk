04a04e19c1d07da3fb23acd9ba0e8e16ca90109d
thomassobolik <ts3bj@virginia.edu>
2018-10-02 17:16:17 -0400

    fast forwarding

--- isa-app/microservices/models.py
+++ isa-app/microservices/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 import datetime
