d94a6391afe3b7266c65a14c9e707bd5bc0628c0
Dylan Cooper <daedrika@gmail.com>
2018-10-19 14:34:13 -0400

    Fix view_layer views

--- view_layer/views.py
+++ view_layer/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
 
-def samplePack_details(pk, template_name='pack_detail.html')
+def samplePack_details(request, pk, template_name='pack_detail.html'):
   #get specified sample pack
   pack = get_object_or_404(SamplePack, pk=pk)
   samples = Sample.objects.filter(pack=pk)
   samples_ordered = samples.order_by('name')
-  
+
   data = {
     'pack': pack,
     'samples': samples_ordered
@@ -15,5 +15,5 @@ def samplePack_details(pk, template_name='pack_detail.html')
   return render(request, template_name, data)
 
 
-def home(template_name='home.html')
+def home(request, template_name='home.html'):
   return render(request, template_name)
