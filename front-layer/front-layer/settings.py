7b0d1c35cdc9bbfd70748d59237c910d17d745f1
Dylan <daedrika@gmail.com>
2018-10-20 14:17:04 -0400

    Add/hook up basic homepage, ready for edits

--- front-layer/front-layer/settings.py
+++ front-layer/front-layer/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'front-layer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
