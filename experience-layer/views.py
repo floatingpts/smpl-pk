fc9b2324fad112e629f62302869d99bc8590cacb
user <kellylotr@gmail.com>
2018-10-23 22:21:15 -0400

    add trailing slash to experience layer view

--- experience-layer/views.py
+++ experience-layer/views.py
@@ -6,8 +6,8 @@ from .models import *
 
 def samplePack_details(request, pk):
   # Get specified sample pack.
-  request_samples = urllib.request.Request('models-api:8000/samples_in_packs', method='GET')
-  request_pack = urllib.request.Request('models-api:8000/sample_packs' + pk)
+  request_samples = urllib.request.Request('models-api:8000/samples_in_packs/', method='GET')
+  request_pack = urllib.request.Request('models-api:8000/sample_packs/' + pk + '/')
   json_samples = urllib.request.urlopen(request_samples).read().decode('utf-8')
   json_pack = urllib.request.urlopen(request_pack).read().decode('utf-8')
 
@@ -19,6 +19,6 @@ def samplePack_details(request, pk):
 
 
 def home(request, template_name='home.html'):
-  top_packs = urllib.request.Request('models-api:8000/top5_packs')
+  top_packs = urllib.request.Request('models-api:8000/top5_packs/')
   serializer = SamplePackSerializer(top_packs)
   return JsonResponse(serializer.data)
