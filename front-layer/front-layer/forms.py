29014a4a479db1f31b1fbb90694ccb3b578c4cbd
user <kellylotr@gmail.com>
2018-11-12 18:33:37 -0500

    initial work on front-end layer views

--- front-layer/front-layer/forms.py
+++ front-layer/front-layer/forms.py
@@ -2,10 +2,14 @@ from django import forms
 
 
 class ListingForm(forms.Form):
-    
+    name = forms.CharField(label='Name', max_length=50)
+    description = forms.TextField(label='Description', max_length=1000)
+    price = models.DecimalField(label='Price', max_digits=5, decimal_places=2)
+
 
 class MusicianForm(forms.Form):
     username = forms.CharField(label='Username', max_length=25)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(), max_length=100)
     
 
 
--- front-layer/front-layer/views.py
+++ front-layer/front-layer/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+from .forms import *
 import urllib.request
 import urllib.parse
 import json
@@ -25,3 +26,88 @@ def login(request):
     if request.method == 'GET':
         #display login form
         return render(request, 'login.html')
+    # Create new Musician form instance
+    form = MusicianForm(request.POST)
+    
+    # Check if form is valid
+    if not form.is_valid():
+        #Form error, send back to login page with an error ADD ERROR
+        return render('login.html')
+
+    # Get form data
+    username = form.cleaned_data['username']
+    password = form.cleaned_data['password']
+
+    # Get next page. Currently automatically goes to home page
+    next = reverse('home')
+
+    # Send form data to exp layer
+    # ADD ONCE EXP VIEW DONE!!!
+    # ADD CHECKING EXP RESPONSE!!!
+
+    # Can now log user in, set login cookie
+    #ADD ONCE EXP VIEW DONE!!
+    response = HttpResponseRedirect(next)
+    #ADD ONCE EXP VIEW DONE!!
+    return response
+
+def logout(request):
+    response = HttpResponseRedirect(reverse('home'))
+    response.delete_cookie(#'COOKIE NAME HERE')
+    return response
+
+def create_listing(request):
+    #set cookie assigns a string name, use this name to try to get cookie
+    #ADD COOKIE GET
+    #ADD CORRECTION FOR NOT LOGGED IN USER
+    
+    #GET request
+    if request.method == 'GET':
+        #Display form page
+        return render("create_listing.html")
+
+    #Otherwise, create new form instance
+    form = ListingForm(request.POST)
+
+    #Retrieve form data
+    name = form.cleaned_data['name']
+    description = form.cleaned_data['description']
+    price = form.cleaned_data['price']
+
+    #Send form data to exp layer
+    #ADD ONCE EXP DONE!!!
+
+    #Check if exp response says we passed incorrect info
+    #ADD ONCE EXP DONE!!!
+
+    return render("create_listing_success.html")
+
+def create_account(request):
+    if request.method == 'GET':
+        #display signup form
+        return render(request, 'create_account.html')
+    # Create new Musician form instance
+    form = MusicianForm(request.POST)
+    
+    # Check if form is valid
+    if not form.is_valid():
+        #Form error, send back to sign-up page with an error ADD ERROR
+        return render('create_account.html')
+    # Get form data
+    username = form.cleaned_data['username']
+    password = form.cleaned_data['password']
+
+    # Get next page. Currently automatically goes to home page
+    next = reverse('home')
+
+    # Send form data to exp layer
+    # ADD ONCE EXP VIEW DONE!!!
+    # ADD CHECKING EXP RESPONSE!!!
+
+    # Can now log user in, set login cookie
+    #ADD ONCE EXP VIEW DONE!!
+    response = HttpResponseRedirect(next)
+    #ADD ONCE EXP VIEW DONE!!
+    return response
+
+    
