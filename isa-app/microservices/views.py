0afae2ccf35c83150e77d93227009e94dffbe4de
user <kellylotr@gmail.com>
2018-11-16 15:42:56 -0500

    remove tabs in model view

--- isa-app/microservices/views.py
+++ isa-app/microservices/views.py
@@ -14,48 +14,47 @@ import microservices.settings
 
 @csrf_exempt
 def generate_auth(pk):
-	auth = hmac.new(
-		key = settings.SECRET_KEY.encode('utf-8'),
-		msg = os.urandom(32),
-		digestmod = 'sha256',
-	).hexdigest()
-	return auth
+  auth = hmac.new(
+       key = settings.SECRET_KEY.encode('utf-8'),
+       msg = os.urandom(32),
+       digestmod = 'sha256',).hexdigest()
+  return auth
 
 @csrf_exempt
 def musician_list(request):
-	if request.method == 'GET':
-		musicians = Musician.objects.all()
-		serializer = MusicianSerializer(musicians, many=True)
-		return JsonResponse(serializer.data, safe=False)
+  if request.method == 'GET':
+    musicians = Musician.objects.all()
+    serializer = MusicianSerializer(musicians, many=True)
+    return JsonResponse(serializer.data, safe=False)
 
-	elif request.method == 'POST':
-		data = JSONParser().parse(request)
-		serializer = MusicianSerializer(data=data)
-		if serializer.is_valid():
-			serializer.save()
-			return JsonResponse(serializer.data, status=201)
-		return JsonResponse(serializer.errors, status=400)
+  elif request.method == 'POST':
+    data = JSONParser().parse(request)
+    serializer = MusicianSerializer(data=data)
+  if serializer.is_valid():
+    serializer.save()
+    return JsonResponse(serializer.data, status=201)
+  return JsonResponse(serializer.errors, status=400)
 
 @csrf_exempt
 def musician_login(request, user):
-	try:
-            name = user["username"]
-            hashed_pass = user["password"]
-	    musician = Musician.objects.get(username=name, password=hashed_pass)
-	    # Generate random auth string
-	    auth = generate_auth()
-	    # Check that this random string not already used
-	    while(Authenticator.objects.filter(authenticator=auth).exists()):
-	    	auth = generate_auth()
-	    # We now know that string stored in auth is unique,
-	    # so create new authenticator object
-	    new_auth = Authenticator.objects.create(user_id=musician.pk, authenticator=auth, date_created=datetime.date.today())
-	    new_auth.save()
-            serializer = AuthenticatorSerializer(new_auth)
-	    return JsonResponse(serializer.data)
+  try:
+    name = user["username"]
+    hashed_pass = user["password"]
+    musician = Musician.objects.get(username=name, password=hashed_pass)
+    # Generate random auth string
+    auth = generate_auth()
+    # Check that this random string not already used
+    while(Authenticator.objects.filter(authenticator=auth).exists()):
+      auth = generate_auth()
+    # We now know that string stored in auth is unique,
+    # so create new authenticator object
+    new_auth = Authenticator.objects.create(user_id=musician.pk, authenticator=auth, date_created=datetime.date.today())
+    new_auth.save()
+    serializer = AuthenticatorSerializer(new_auth)
+    return JsonResponse(serializer.data)
 
-	except Musician.DoesNotExist:
-	    return HttpResponse(status=404)
+  except Musician.DoesNotExist:
+    return HttpResponse(status=404)
 
 @csrf_exempt
 def musician_detail(request, pk):
