ef7cab7854f740ca513a729fea922adc22c56d90
kellylr <kellylotr@gmail.com>
2018-11-23 11:46:33 -0500

    added initial search indexer

--- isa-app/batchScript.py
+++ isa-app/batchScript.py
@@ -1,1 +1,14 @@
-#pulls messages from Kafka and indexs that in ES
+from kafka import KafkaConsumer
+from elasticsearch import Elasticsearch
+
+#consumer listens to see if any listings added to Kafka queue
+consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+
+#used to add listings to elastic search index
+es = Elasticsearch(['es'])
+
+for message in consumer:
+    # add listing to elastic search
+    es.index(index='listing_index', doc_type='listing', id=message['id'], body=message)
+{'created': True, '_version': 1, '_shards': {'successful': 1, 'total': 2, 'failed': 0}, '_index': 'listing_index', '_id': '42', '_type': 'listing'}
+    es.indices.refresh(index="listing_index")
