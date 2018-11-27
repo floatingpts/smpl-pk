03db0d3d38b418717f3ede1a72cb0a40ea7c5383
kellylr <kellylotr@gmail.com>
2018-11-27 15:17:54 -0500

    add skeleton to batchScript

--- isa-app/batchScript.py
+++ isa-app/batchScript.py
@@ -1,1 +1,9 @@
-#pulls messages from Kafka and indexs that in ES
+# Pulls messages from Kafka and indexes that in ES
+from kafka import KafkaConsumer
+while True:
+    consumer = KafkaConsumer('sample-pack-listings', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+    for message in consumer:
+        listing = json.loads((message.value).decode('utf-8'))
+        # TODO: How to figure out if listing is new? Does KafkaConsumer do this automatically?
+        if listing_is_new:
+            # Make request to ElasticSearch container with listing in body
