9302f25390bed180f91534417dd1382bd86af3ce
kellylr <kellylotr@gmail.com>
2018-11-28 22:18:08 -0500

    Dylan's code for batch.py

--- isa-app/batchScript.py
+++ isa-app/batchScript.py
@@ -2,8 +2,7 @@
 from kafka import KafkaConsumer
 while True:
     consumer = KafkaConsumer('sample-pack-listings', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+    # Exception handling with Docker Compose
     for message in consumer:
         listing = json.loads((message.value).decode('utf-8'))
-        # TODO: How to figure out if listing is new? Does KafkaConsumer do this automatically?
-        if listing_is_new:
-            # Make request to ElasticSearch container with listing in body
+
