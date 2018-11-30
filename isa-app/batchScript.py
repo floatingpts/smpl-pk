7a0023c389e8b5620c38f46d6b901d3596e5f001
thomassobolik <ts3bj@virginia.edu>
2018-11-29 23:37:56 -0500

    fixed some errors in front-end & got batch loop running

--- isa-app/batchScript.py
+++ isa-app/batchScript.py
@@ -1,8 +1,6 @@
 # Pulls messages from Kafka and indexes that in ES
 from kafka import KafkaConsumer
-while True:
-    consumer = KafkaConsumer('sample-pack-listings', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
-    # Exception handling with Docker Compose
-    for message in consumer:
-        listing = json.loads((message.value).decode('utf-8'))
-
+import json
+consumer = KafkaConsumer('sample-pack-listings', group_id='listing-indexer', bootstrap_servers=['kafka:9092'], api_version=('0.9'))
+for message in consumer:
+    listing = json.loads((message.value).decode('utf-8'))
