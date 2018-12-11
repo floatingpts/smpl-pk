e9e2d2d0958fbe40872476b0aa8e4df6b110e892
thomassobolik <ts3bj@virginia.edu>
2018-12-11 16:44:27 -0500

    added integration test for create-account function

--- isa-app/testIntegration.py
+++ isa-app/testIntegration.py
@@ -27,7 +27,36 @@ class Integration(unittest.TestCase):
         search_form.submit()
         # Check that 'Piano sound effects' in results
         assert "Piano sound effects" in driver.page_source
+
+    def test_signup(self):
+        driver = self.driver
+        # get homepage and assert success
+        driver.get('http://web:8000')
+        self.assertEqual('Homepage', driver.title)
+        # navigate to login page and assert success
+        driver.get('http://web:8000/login/')
+        self.assertEqual('Login', driver.title)
+        # navigate to create account page and assert success
+        driver.get('http://web:8000/create_account/')
+        self.assertEqual('Sign up', driver.title)
         
+        # test call to create_account service with sample form data
+
+        # locate username box
+        signup_form = driver.find_element_by_name('username')
+        signup_form.send_keys('tom')
+        # locate password box
+        signup_form = driver.find_element_by_name('password')
+        signup_form.send_keys('password')
+        # locate email box
+        signup_form = driver.find_element_by_name('email')
+        signup_form.send_keys('tom@gmail.com')       
+        # submit the form
+        signup_form.submit()
+
+        # check that we successfully returned to the homepage
+        self.assertEqual('Homepage', driver.title)
+
     def tearDown(self):
         self.driver.close()
         
