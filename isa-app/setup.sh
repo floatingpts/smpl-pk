4bb1f994fedacab7624382c7df46584444b453a6
Dylan <daedrika@gmail.com>
2018-12-11 22:19:35 -0500

    Try sleeping between creating mysql container and executing commands.

--- isa-app/setup.sh
+++ isa-app/setup.sh
@@ -1,3 +1,3 @@
-echo "CREATE USER 'www'@'%' identified by '$3cureUS'; \
+echo "create user 'www'@'%' identified by '$3cureUS'; \
     create database cs4501 character set utf8; \
     grant all on cs4501.* to 'www'@'%';" | mysql -uroot -p'$3cureUS'
